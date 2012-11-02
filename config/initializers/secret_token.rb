c79412fe8e316b4d56f848a5912dccba026518bb
Gabriel Peal <gpeal@u.northwestern.edu>
2012-11-02 00:05:15 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaesarScraper::Application.config.secret_token = '072c4502ad2b6e606f9c00988de0b022f9ab72a954136582a4f66a2baa793df15e2ff47cfc88349ae56a6e4910c70baadd94b4c3c3c4c4b40daab0ee9ea43bab'
