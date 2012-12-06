2f30e3260cedebaac4e10679e9034598478b82b7
Gabriel Peal <gpeal@u.northwestern.edu>
2012-12-06 16:30:13 -0600

    Improved chart visibility

--- app/assets/javascripts/data.js
+++ app/assets/javascripts/data.js
@@ -4,6 +4,43 @@ var organizedSections = {};
 var quarters = [];
 
 $(document).ready(function() {
+  /**
+       * Highcharts plugin for setting a lower opacity for other series than the one that is hovered
+       * in the legend
+       */
+      (function (Highcharts) {
+          var each = Highcharts.each;
+
+          Highcharts.wrap(Highcharts.Legend.prototype, 'renderItem', function (proceed, item) {
+
+              proceed.call(this, item);
+
+
+              var series = this.chart.series,
+                  element = item.legendGroup.element;
+
+              element.onmouseover = function () {
+                 each(series, function (seriesItem) {
+                      if (seriesItem !== item) {
+                          each(['group', 'markerGroup'], function (group) {
+                              seriesItem[group].attr('opacity', 0.25);
+                          });
+                      }
+                  });
+              }
+              element.onmouseout = function () {
+                 each(series, function (seriesItem) {
+                      if (seriesItem !== item) {
+                          each(['group', 'markerGroup'], function (group) {
+                              seriesItem[group].attr('opacity', 1);
+                          });
+                      }
+                  });
+              }
+
+          });
+      }(Highcharts));
+
   requestData();
 });
 
@@ -34,18 +71,18 @@ function loadData(data) {
     var keys = Object.keys(organizedSections);
     for(var section_id in keys) {
       var section_name = keys[section_id];
-      var series = {};
-      series.name = section_name;
-      series.data = [];
+      var courseSeries_tmp = {};
+      courseSeries_tmp.name = section_name;
+      courseSeries_tmp.data = [];
       for(var j in organizedSections[section_name]) {
         var section = organizedSections[section_name][j].section
-        series.data.push([quarterName(section), section.course]);
+        courseSeries_tmp.data.push([quarterName(section), section.course]);
       }
-      courseSeries.push(series);
+      courseSeries.push(courseSeries_tmp);
     }
   }
 
-  refreshChart(charts.course, courseSeries, 'Course Rating', quarters, 'Score');
+  refreshChart(charts.course, 'chart-course', courseSeries, 'Course Rating', quarters, 'Score');
 }
 
 function sectionName(section) {
@@ -56,10 +93,10 @@ function quarterName(section) {
   return [section.quarter.title, section.year.title].join(' ');
 }
 
-function refreshChart(chart, series, title, categories, yTitle) {
+function refreshChart(chart, id, series, title, categories, yTitle) {
   chart = new Highcharts.Chart({
     chart: {
-      renderTo: 'container',
+      renderTo: id,
       type: 'spline'
     },
     credits: {
