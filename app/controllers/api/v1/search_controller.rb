99d00d5fad26a680d4173b6cc2ff66cbaf0aa6fd
Gabriel Peal <gpeal@u.northwestern.edu>
2012-12-22 17:44:56 -0500

    Backend now returns overview

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -13,12 +13,30 @@ class Api::V1::SearchController < ApplicationController
 
     first_year = @sections[0].year unless @sections.nil?
     first_quarter = @sections[0].quarter unless @sections.nil?
+    overview = {}
     @sections.each do |section|
       first_year = section.year if section.year < first_year
       first_quarter = section.quarter if (section.year <= first_year &&
                                           section.quarter < first_quarter)
+      key = "#{section.professor.to_s} #{section.to_s}"
+      overview[key] = {professor: section.professor.to_s, title: section.title.to_s, instruction: [], course: [], learned: [], stimulated: [], challenged: [], hours: []} if overview[key].nil?
+      overview[key][:instruction].push(section.instruction)
+      overview[key][:course].push(section.course)
+      overview[key][:learned].push(section.learned)
+      overview[key][:stimulated].push(section.stimulated)
+      overview[key][:challenged].push(section.challenged)
+      overview[key][:hours].push(section.hours)
     end
 
-    render :json => {:sections => @sections, :xRange => {:firstQuarter => first_quarter, :firstYear => first_year}}
+    overview.keys.each do |key|
+      overview[key][:instruction] = overview[key][:instruction].inject{ |sum, el| sum + el }.to_f / overview[key][:instruction].size
+      overview[key][:course] = overview[key][:course].inject{ |sum, el| sum + el }.to_f / overview[key][:course].size
+      overview[key][:learned] = overview[key][:learned].inject{ |sum, el| sum + el }.to_f / overview[key][:learned].size
+      overview[key][:stimulated] = overview[key][:stimulated].inject{ |sum, el| sum + el }.to_f / overview[key][:stimulated].size
+      overview[key][:challenged] = overview[key][:challenged].inject{ |sum, el| sum + el }.to_f / overview[key][:challenged].size
+      overview[key][:hours] = overview[key][:hours].inject{ |sum, el| sum + el }.to_f / overview[key][:hours].size
+    end
+
+    render :json => {:sections => @sections, :xRange => {:firstQuarter => first_quarter, :firstYear => first_year}, :overview => overview}
   end
 end
