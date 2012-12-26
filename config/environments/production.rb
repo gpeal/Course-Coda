1adc85e091b9e8b02b07447d5915a32dc3e4ce00
Gabriel Peal <gpeal@u.northwestern.edu>
2012-12-25 20:59:23 -0500

    Added professors js to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ CaesarScraper::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( home/data.js home/feedback.js home/highcharts.js home/highscharts.js-more home/sidebar.js)
   config.assets.precompile += %w( courses/courses.js)
+  config.assets.precompile += %w( professors/professors.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
