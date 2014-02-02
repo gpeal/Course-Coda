474b5f6e6893fc34d4e2b09e7a4bd38cdb8426fb
Gabriel Peal <gpeal@u.northwestern.edu>
2014-02-02 00:52:48 -0800

    Only allow signups for @northwestern and @gpeal accounts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, presence: true, if: :is_allowed_domain?
+
+  def is_allowed_domain?
+    unless %w(northwestern.edu gpeal.com).include?(email[email.index('@')+1..-1] )
+      errors.add(:email, "Currently only @northwestern accounts are currently allowed.")
+    end
+  end
 end
