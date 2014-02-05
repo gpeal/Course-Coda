48cf991ba6d12ff146df09eb6453ec1debd10c8c
Gabriel Peal <gpeal@u.northwestern.edu>
2014-02-04 23:11:03 -0800

    Send pageview to google analytics

--- app/assets/javascripts/global/google_analytics.js
+++ app/assets/javascripts/global/google_analytics.js
@@ -4,3 +4,4 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-47789870-1', 'nuctecs.com');
+ga('send', 'pageview');
