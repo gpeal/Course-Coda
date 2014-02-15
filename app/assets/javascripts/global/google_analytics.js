94c06bff6ed07d4bde28d8faa519b5f8952b4c43
Gabriel Peal <gpeal@u.northwestern.edu>
2014-02-15 15:45:20 -0800

    Enabled ga advanced link attribution

--- app/assets/javascripts/global/google_analytics.js
+++ app/assets/javascripts/global/google_analytics.js
@@ -4,5 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-47789870-1', 'nuctecs.com');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
-console.log('ga init');
